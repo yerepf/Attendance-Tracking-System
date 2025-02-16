89d0ad9af08f10fbd2d439942f8db50c632f7cdf
CesarCruz18 <edelacrucru@gmail.com>
2025-02-16 10:11:13 -0400

    Created main module. @CesarCrz18

--- winforms/ATS-WinForms/ATS-WinForms/Menu.Designer.cs
+++ winforms/ATS-WinForms/ATS-WinForms/Menu.Designer.cs
@@ -28,12 +28,242 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.panelChildForm = new System.Windows.Forms.Panel();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.panelSideMenu = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnReportes = new System.Windows.Forms.Button();
+            this.btnDistritos = new System.Windows.Forms.Button();
+            this.btnEstudiantes = new System.Windows.Forms.Button();
+            this.btnIsntituciones = new System.Windows.Forms.Button();
+            this.btnUsuaario = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelChildForm.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.panelSideMenu.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelChildForm
+            // 
+            this.panelChildForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.panelChildForm.Controls.Add(this.pictureBox9);
+            this.panelChildForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelChildForm.Location = new System.Drawing.Point(250, 0);
+            this.panelChildForm.Name = "panelChildForm";
+            this.panelChildForm.Size = new System.Drawing.Size(758, 561);
+            this.panelChildForm.TabIndex = 9;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox9.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox9.Image")));
+            this.pictureBox9.Location = new System.Drawing.Point(29, 31);
+            this.pictureBox9.MinimumSize = new System.Drawing.Size(706, 493);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(706, 493);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox9.TabIndex = 2;
+            this.pictureBox9.TabStop = false;
+            // 
+            // panelSideMenu
+            // 
+            this.panelSideMenu.AutoScroll = true;
+            this.panelSideMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(1)))), ((int)(((byte)(28)))));
+            this.panelSideMenu.Controls.Add(this.btnExit);
+            this.panelSideMenu.Controls.Add(this.btnReportes);
+            this.panelSideMenu.Controls.Add(this.btnDistritos);
+            this.panelSideMenu.Controls.Add(this.btnEstudiantes);
+            this.panelSideMenu.Controls.Add(this.btnIsntituciones);
+            this.panelSideMenu.Controls.Add(this.btnUsuaario);
+            this.panelSideMenu.Controls.Add(this.panelLogo);
+            this.panelSideMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSideMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelSideMenu.Name = "panelSideMenu";
+            this.panelSideMenu.Size = new System.Drawing.Size(250, 561);
+            this.panelSideMenu.TabIndex = 8;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(21)))), ((int)(((byte)(32)))));
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(22)))), ((int)(((byte)(34)))));
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Roboto", 12F);
+            this.btnExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.Location = new System.Drawing.Point(0, 516);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnExit.Size = new System.Drawing.Size(250, 45);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Cerrar sesión";
+            this.btnExit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnReportes
+            // 
+            this.btnReportes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReportes.FlatAppearance.BorderSize = 0;
+            this.btnReportes.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(21)))), ((int)(((byte)(32)))));
+            this.btnReportes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(22)))), ((int)(((byte)(34)))));
+            this.btnReportes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReportes.Font = new System.Drawing.Font("Roboto", 12F);
+            this.btnReportes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.btnReportes.Image = ((System.Drawing.Image)(resources.GetObject("btnReportes.Image")));
+            this.btnReportes.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReportes.Location = new System.Drawing.Point(0, 272);
+            this.btnReportes.Name = "btnReportes";
+            this.btnReportes.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnReportes.Size = new System.Drawing.Size(250, 45);
+            this.btnReportes.TabIndex = 8;
+            this.btnReportes.Text = "Reportes";
+            this.btnReportes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReportes.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnReportes.UseVisualStyleBackColor = true;
+            // 
+            // btnDistritos
+            // 
+            this.btnDistritos.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDistritos.FlatAppearance.BorderSize = 0;
+            this.btnDistritos.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(21)))), ((int)(((byte)(32)))));
+            this.btnDistritos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(22)))), ((int)(((byte)(34)))));
+            this.btnDistritos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDistritos.Font = new System.Drawing.Font("Roboto", 12F);
+            this.btnDistritos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.btnDistritos.Image = ((System.Drawing.Image)(resources.GetObject("btnDistritos.Image")));
+            this.btnDistritos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDistritos.Location = new System.Drawing.Point(0, 227);
+            this.btnDistritos.Name = "btnDistritos";
+            this.btnDistritos.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnDistritos.Size = new System.Drawing.Size(250, 45);
+            this.btnDistritos.TabIndex = 6;
+            this.btnDistritos.Text = "Distritos";
+            this.btnDistritos.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDistritos.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDistritos.UseVisualStyleBackColor = true;
+            // 
+            // btnEstudiantes
+            // 
+            this.btnEstudiantes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEstudiantes.FlatAppearance.BorderSize = 0;
+            this.btnEstudiantes.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(21)))), ((int)(((byte)(32)))));
+            this.btnEstudiantes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(22)))), ((int)(((byte)(34)))));
+            this.btnEstudiantes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEstudiantes.Font = new System.Drawing.Font("Roboto", 12F);
+            this.btnEstudiantes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.btnEstudiantes.Image = ((System.Drawing.Image)(resources.GetObject("btnEstudiantes.Image")));
+            this.btnEstudiantes.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEstudiantes.Location = new System.Drawing.Point(0, 182);
+            this.btnEstudiantes.Name = "btnEstudiantes";
+            this.btnEstudiantes.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnEstudiantes.Size = new System.Drawing.Size(250, 45);
+            this.btnEstudiantes.TabIndex = 5;
+            this.btnEstudiantes.Text = "Estudiantes";
+            this.btnEstudiantes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEstudiantes.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnEstudiantes.UseVisualStyleBackColor = true;
+            // 
+            // btnIsntituciones
+            // 
+            this.btnIsntituciones.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnIsntituciones.FlatAppearance.BorderSize = 0;
+            this.btnIsntituciones.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(21)))), ((int)(((byte)(32)))));
+            this.btnIsntituciones.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(22)))), ((int)(((byte)(34)))));
+            this.btnIsntituciones.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIsntituciones.Font = new System.Drawing.Font("Roboto", 12F);
+            this.btnIsntituciones.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.btnIsntituciones.Image = ((System.Drawing.Image)(resources.GetObject("btnIsntituciones.Image")));
+            this.btnIsntituciones.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnIsntituciones.Location = new System.Drawing.Point(0, 137);
+            this.btnIsntituciones.Name = "btnIsntituciones";
+            this.btnIsntituciones.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnIsntituciones.Size = new System.Drawing.Size(250, 45);
+            this.btnIsntituciones.TabIndex = 3;
+            this.btnIsntituciones.Text = "Isntituciones";
+            this.btnIsntituciones.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnIsntituciones.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnIsntituciones.UseVisualStyleBackColor = true;
+            // 
+            // btnUsuaario
+            // 
+            this.btnUsuaario.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnUsuaario.FlatAppearance.BorderSize = 0;
+            this.btnUsuaario.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(21)))), ((int)(((byte)(32)))));
+            this.btnUsuaario.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(22)))), ((int)(((byte)(34)))));
+            this.btnUsuaario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUsuaario.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsuaario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.btnUsuaario.Image = ((System.Drawing.Image)(resources.GetObject("btnUsuaario.Image")));
+            this.btnUsuaario.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnUsuaario.Location = new System.Drawing.Point(0, 92);
+            this.btnUsuaario.Name = "btnUsuaario";
+            this.btnUsuaario.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnUsuaario.Size = new System.Drawing.Size(250, 45);
+            this.btnUsuaario.TabIndex = 1;
+            this.btnUsuaario.Text = "Usuarios";
+            this.btnUsuaario.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnUsuaario.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnUsuaario.UseVisualStyleBackColor = true;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Controls.Add(this.pictureBox1);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(250, 92);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(36, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(159, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1008, 561);
+            this.Controls.Add(this.panelChildForm);
+            this.Controls.Add(this.panelSideMenu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Menu_FormClosing);
+            this.panelChildForm.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            this.panelSideMenu.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelChildForm;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.Panel panelSideMenu;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnReportes;
+        private System.Windows.Forms.Button btnDistritos;
+        private System.Windows.Forms.Button btnEstudiantes;
+        private System.Windows.Forms.Button btnIsntituciones;
+        private System.Windows.Forms.Button btnUsuaario;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
